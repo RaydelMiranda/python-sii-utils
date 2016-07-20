add60d8458be30048f86bfcd2cb6357ab920b634
Leonhard Weber <leonhard.weber@voipir.cl>
2016-07-20 16:54:02 -0400

    Release Candidate 2.
    * Added version displaying.
    * Fixed location for configuration file template.
    * Bumped version to final v1.0.0.
    * Bumped python-sii dependency version to final v1.0.0.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 cfg = {
     'name'             : 'python-sii-utils',
     'long_description' : __doc__,
-    'version'          : '1.0.0.dev2016072000',
+    'version'          : '1.0.0',
     'packages'         : find_packages('src'),
     'package_dir'      : {'': 'src'},
 
@@ -14,7 +14,7 @@ cfg = {
 
     'install_requires': [
         'docopt     >= 0.6.2',
-        'python-sii >= 1.0.0.dev2016072000',
+        'python-sii >= 1.0.0',
     ],
 
     'entry_points': {
