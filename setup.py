adf7719061d60c589ac46e94d7722eb7721ea66d
Leonhard Weber <leonhard.weber@voipir.cl>
2016-09-12 18:25:14 -0300

    Releasing v1.1.0~2016091200 (testing).
    * Fixed broken case switch in LCV utils.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 cfg = {
     'name'             : 'python-sii-utils',
     'long_description' : __doc__,
-    'version'          : '1.1.0.dev2016080900',
+    'version'          : '1.1.0.dev2016091200',
     'packages'         : find_packages('src'),
     'package_dir'      : {'': 'src'},
 
